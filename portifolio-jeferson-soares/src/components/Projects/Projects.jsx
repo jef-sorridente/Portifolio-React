492b205637d73d5f116dd99f922507ae3fbb2966
Jeferson Soares <jpadilha53@gmail.com>
2023-06-02 18:15:07 -0300

    Teste de Conteúdo

--- portifolio-jeferson-soares/src/components/Projects/Projects.jsx
+++ portifolio-jeferson-soares/src/components/Projects/Projects.jsx
@@ -3,7 +3,22 @@ import "./Projects.css";
 // Icons
 import { AiOutlineArrowsAlt } from "react-icons/ai";
 
+//Hooks
+import { useState } from "react";
+
 const Projects = () => {
+  const [expandedIndex, setExpandedIndex] = useState(null);
+
+  const arrayProjects = [
+    { id: 1, title: "MiniBlog React", content: "Conteúdo do item 1" },
+    { id: 2, title: "Item 2", content: "Conteúdo do item 2" },
+    { id: 3, title: "Item 3", content: "Conteúdo do item 3" },
+    { id: 4, title: "Item 4", content: "Conteúdo do item 4" },
+  ];
+
+  const handleExpand = (index) => {
+    setExpandedIndex(index === expandedIndex ? null : index);
+  };
   return (
     <div className="projects" id="project">
       <div className="projects-item">
@@ -15,51 +30,22 @@ const Projects = () => {
           <div className="project-img">Imagem</div>
         </div>
       </div>
-      <div className="projects-item">
-        <div className="projects-container">
-          <div className="projects-title">
-            <h3>Projeto 2</h3>
-            <AiOutlineArrowsAlt />
+      {arrayProjects.map((item, index) => (
+        <div
+          key={item.id}
+          className={`item ${
+            expandedIndex === index ? "projects-item" : "projects-item"
+          }`}
+        >
+          <div className="projects-container">
+            <div className="projects-title">
+              <h3>{item.title}</h3>
+              <AiOutlineArrowsAlt onClick={() => handleExpand(index)} />
+            </div>
+            {expandedIndex === index && <div>{item.content}</div>}
           </div>
-          <div className="project-img">Imagem</div>
         </div>
-      </div>
-      <div className="projects-item">
-        <div className="projects-container">
-          <div className="projects-title">
-            <h3>Projeto 3</h3>
-            <AiOutlineArrowsAlt />
-          </div>
-          <div className="project-img">Imagem</div>
-        </div>
-      </div>
-      <div className="projects-item">
-        <div className="projects-container">
-          <div className="projects-title">
-            <h3>Projeto 4</h3>
-            <AiOutlineArrowsAlt />
-          </div>
-          <div className="project-img">Imagem</div>
-        </div>
-      </div>
-      <div className="projects-item">
-        <div className="projects-container">
-          <div className="projects-title">
-            <h3>Projeto 5</h3>
-            <AiOutlineArrowsAlt />
-          </div>
-          <div className="project-img">Imagem</div>
-        </div>
-      </div>
-      <div className="projects-item">
-        <div className="projects-container">
-          <div className="projects-title">
-            <h3>Projeto 6</h3>
-            <AiOutlineArrowsAlt />
-          </div>
-          <div className="project-img">Imagem</div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
